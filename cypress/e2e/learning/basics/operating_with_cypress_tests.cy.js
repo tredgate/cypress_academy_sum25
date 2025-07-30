2744cf56aa8b7e5f46df7d7c77876ffdecd093d2
Petr Fifka <petrfifk@gmail.com>
2025-07-30 21:09:15 +0200

    Cypress introduction

---
+++ cypress/e2e/learning/basics/operating_with_cypress_tests.cy.js
@@ -0,0 +1,17 @@
+// operating_with_cypress_tests.cy.js
+// cypress/e2e/learning/basics
+
+describe("Operating with Cypress", () => {
+  it.only("Login Test", () => {
+    cy.visit("https://tredgate.com/pmtool");
+    cy.get("#username").type("cypress_zima_2024");
+    cy.get("#password").type("Zima2024Cypress");
+    cy.get(".btn").click();
+  });
+
+  // ? it.skip - přeskakuje test při spuštění
+  it.skip("Test failure - element not present", () => {
+    cy.visit("https://tredgate.com/pmtool");
+    cy.get("#username2").type("cypress_zima_2024");
+  });
+});
